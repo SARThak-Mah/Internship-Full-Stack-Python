d418877b45f8190c83c5dd7c4c1766c8f2aa7e55
SARThak Mahurkar <sarthakdvm@gmail.com>
2026-06-22 01:01:06 +0530

    Refactor imports and update root rendering
    Updated imports and rendering components in main.jsx.

--- main.jsx
+++ main.jsx
@@ -1,13 +1,18 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-// import App from "./App.jsx";
-// import First from "./First.jsx";
-//import MyHooks from "./MyHooks.jsx";
-// import MyEvent from "./MyEvent.jsx";
-// import List from "./List.jsx";
-//import LoginPage from "./LoginPage.jsx";
+import MyApp from "./MyApp.jsx";
+import App from "./App.jsx";
+import First from "./First.jsx";
+import MyHooks from "./MyHooks.jsx";
+import MyEvent from "./MyEvent.jsx";
+import List from "./List.jsx";
+import LoginPage from "./LoginPage.jsx";
 import Api from "./Api.jsx";
+import MyApi from "./MyApi.jsx";
+import MyApp from "./MyApp.jsx";
+import Myapp2 from "./Myapp2.jsx";
+import Form from "./Form.jsx";
 
 /*createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -30,4 +35,10 @@ import Api from "./Api.jsx";
 
 //createRoot(document.getElementById("root")).render(<LoginPage />);
 
-createRoot(document.getElementById("root")).render(<Api />);
+// createRoot(document.getElementById("root")).render(<Api />);
+
+// createRoot(document.getElementById("root")).render(<MyApi />);
+
+createRoot(document.getElementById("root")).render(<Myapp2 />);
+
+// createRoot(document.getElementById("root")).render(<Form />);
